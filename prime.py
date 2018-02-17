cc43940638867f5458556be36cfa398bda5cac34
Nighmared <jonas.konrath@icloud.com>
2018-02-17 13:51:41 +0100

    speed improvements

--- prime.py
+++ prime.py
@@ -1,9 +1,9 @@
-PRIME = [2,3,5,7]
+PRIME = [2]
 def goforit(range=100000):
 	'''Use this command to search for primes.
 	the range argument defines the upper limit
 	default range is 100'000
-	Have fun, ©Nighmared
+	Have fun, (c) Nighmared
 	----
 	nighmared.github.io'''
 	x = 1
@@ -13,9 +13,12 @@ def goforit(range=100000):
 		counter +=1
 		isprime = True
 		TOTAL = PRIME
-		for y in limit(x):
-			if x%y ==0 and x != y:
+		for y in PRIME:
+			if y*y>x:
+				break
+			if x%y ==0:
 				isprime = False
+				break
 		if isprime and x !=1 and x!=0 and x not in PRIME:
 			PRIME.append(x)
 		if counter>(range/200):
@@ -39,3 +42,7 @@ def export(list,name):
 		file.write(str(x)+'\n')
 	file.close()
 	print('export finished ({})'.format(name))
+
+
+
+goforit()
